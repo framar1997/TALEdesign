1b05a7d0f7ca33b3d38205504317eb0bc2cc9865
framar1997 <122021863+framar1997@users.noreply.github.com>
2024-03-26 12:49:39 +0100

    Add files via upload

---
+++ core_functions/GetToxicity.R
@@ -0,0 +1,14 @@
+#' GetToxicity: this function extracts the probability of toxicity of DL (i,j) from the toxicity matrix
+#'
+#' @param i: level of the first drug
+#' @param j: level of the second drug drug
+#' @param toxicity_matrix: matrix of toxicities
+#' @param return the probability of toxicity at DL (i,j)
+#'
+
+GetToxicity = function(i, j, toxicity_matrix)
+{
+  x = which(colnames(toxicity_matrix) == toString(i) )
+  y = which(rownames(toxicity_matrix) == toString(j) )
+  return(toxicity_matrix[y,x])
+}
